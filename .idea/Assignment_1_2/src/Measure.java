fe1f0a6d780fbb08b9f9168426131b8cd3bee557
Welcome <roshinif@metropolia.fi>
2025-08-26 10:46:06 +0300

    Assignment_1_2

--- .idea/Assignment_1_2/src/Measure.java
+++ .idea/Assignment_1_2/src/Measure.java
@@ -1,17 +1,17 @@
-import  java.util.Scanner;
+import java.util.Scanner;
 public class Measure {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the weight in grams: ");
-        double weightGram = scanner.nextDouble();
+        double weightInGrams = scanner.nextDouble();
 
-        double leiviskä = Math.floor(weightGram/(13.28*32*20));
-        double naula = Math.floor((weightGram/(13.28*32*20)-leiviskä )* 20) ;
-        double luoti = (((weightGram/(13.28*32*20)-leiviskä )* 20)-naula)*32 ;
+        double leiviskä = Math.floor(weightInGrams / (13.28 * 32 * 20));
+        double naula = Math.floor((weightInGrams / (13.28 * 32 * 20) - leiviskä) * 20);
+        double luoti = (((weightInGrams / (13.28 * 32 * 20) - leiviskä) * 20) - naula) * 32;
 
-        System.out.printf("%.0f gram is %.0f leiviska, %.0f30 of naula, and %.2f luoti.",weightGram,leiviskä,naula,luoti);
+        System.out.printf("%.0f gram is %.0f leiviska, %.0f of naula, and %.2f luoti.", weightInGrams, leiviskä, naula, luoti);
 
-
+        scanner.close();
     }
 }
